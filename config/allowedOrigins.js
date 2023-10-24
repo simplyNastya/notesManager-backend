4f511fe67ebaa3474a5ac5d310619327e2933f66
Anastasiia Koretska <koretska.anastasiia@gmail.com>
2023-10-24 16:38:05 +0200

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'https://notes-manager-frontend.onrender.com'
+    'https://notesmanager.onrender.com'
 ]
 
 module.exports = allowedOrigins
